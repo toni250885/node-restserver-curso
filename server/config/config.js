b234f73534834fc7d0c9a76a53e696e180dde3a8
Toni <antonio.martinez.casfid@gmail.com>
2019-10-25 09:28:39 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 //PUERTO
 //=====================================================
 process.env.PORT = process.env.PORT || 3000;
+
+//=====================================================
+//ENTORNO
+//=====================================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=====================================================
+//BASE DE DATOS
+//=====================================================
+
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb+srv://toni:martin25081985@cluster0-6e2k0.mongodb.net/test?retryWrites=true&w=majority';
+    
+}
+
+process.env.URLDB = urlDB;
