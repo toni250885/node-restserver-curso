63c8a0ecbdf726f4e42c71d803f25af015d5c517
Toni <antonio.martinez.casfid@gmail.com>
2019-10-29 11:59:53 +0100

    Subida de archivos

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,8 @@ app.use( require('./usuario.js'));
 app.use( require('./login.js'));
 app.use( require('./categoria.js'));
 app.use( require('./producto.js'));
+app.use( require('./upload.js'));
+app.use( require('./imagenes.js'));
 
 
 module.exports = app;
